8bef10d6ea9d2572c5be654ec06263f109b7edb5
daiki.sakaue <banshangdahui826@gmail.com>
2019-09-28 08:08:11 +0000

    2019/09/28

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  
   def create
     user = @current_user
-    micropost = current_user.microposts.find_by(micropost_id: @micropost_id)
+    micropost = user.microposts.find_by(micropost_id: @micropost_id)
     user.like(micropost)
-    falsh[:success] = 'お気に入り登録しました。'
+    flash[:success] = 'お気に入り登録しました。'
     redirect_to favorites/index
   end
 
   def destroy
     user = @current_user
-    micropost = current_user.microposts.find_by(micropost_id: @micropost_id)
+    micropost = user.microposts.find_by(micropost_id: @micropost_id)
     user.dislike(micropost)
-    falsh.now[:danger] = 'お気に入り登録を削除しました。'
+    flash.now[:danger] = 'お気に入り登録を削除しました。'
     render favorites/index
   end
 end
